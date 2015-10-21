25d887e7cc1e0c136cd433998efe079c7933e72c
vnshetty88 <vnagarajshetty@gmail.com>
2015-10-21 15:30:34 +0530

    User2: adding file

--- app/src/main/java/com/vnshetty/gitsample1/MainActivity.java
+++ app/src/main/java/com/vnshetty/gitsample1/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         //First comment
+        //Second Comment
     }
 
     @Override
---
+++ app/src/main/java/com/vnshetty/gitsample1/User2.java
@@ -0,0 +1,10 @@
+package com.vnshetty.gitsample1;
+
+/*
+Project     : BB-8
+Author      : Nagaraja Shetty on 21-10-2015 
+Copyright   : Copyright © 2015, NDTV Worldwide  Written under contract by Robosoft Technologies Pvt. Ltd.
+History     : NA
+*/
+public class User2 {
+}
